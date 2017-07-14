265b8cd26a99aedb52a2cf0fc2bc6c7d7fe9445c
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-07-15 00:42:17 +0530

    download manager download file by url

--- app/src/main/java/com/appbusters/robinkamboj/filedownloader/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/filedownloader/MainActivity.java
@@ -1,13 +1,18 @@
 package com.appbusters.robinkamboj.filedownloader;
 
 import android.Manifest;
+import android.app.DownloadManager;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "http://techslides.com/demos/sample-videos/small.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.one:{
-
+                downloadUsingDownloadManager(URL);
                 break;
             }
             case R.id.two:{
@@ -40,5 +45,18 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
         }
+    }
+
+    public void downloadUsingDownloadManager(String url){
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setTitle("Downloading File");
+        request.setDescription("Your file is downloading..");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "download.mp4");
+
+        //get download manager and enqueue file
+        DownloadManager manager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
 }
