f8bc05a5969f27e5a188151cc7ab73894085e03b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-07-14 22:14:53 +0530

    basic layout

--- app/src/main/java/com/appbusters/robinkamboj/filedownloader/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/filedownloader/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +17,27 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) || (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)){
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 5);
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.one:{
+
+                break;
+            }
+            case R.id.two:{
+
+                break;
+            }
+            case R.id.three:{
+
+                break;
+            }
+            case R.id.four:{
+
+                break;
             }
         }
     }
